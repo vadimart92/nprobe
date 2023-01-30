63bbde26f7614278b321aee8ac20fa1906449efb
Vadym Artemchuk <vadimart92@gmail.com>
2023-01-30 22:11:14 +0200

    initial

---
+++ NProbe.SmartDebugger/StartupHook.cs
@@ -0,0 +1,8 @@
+﻿// ReSharper disable once CheckNamespace
+public class StartupHook
+{
+	//https://github.com/dotnet/runtime/blob/main/docs/design/features/host-startup-hook.md
+	public static void Initialize() {
+		
+	}
+}
